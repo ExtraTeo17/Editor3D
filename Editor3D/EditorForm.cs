1f5037cf76cae2fc948fca914a3917fbca64bc00
Robert Pniewski <extrateo17@gmail.com>
2021-02-01 16:40:29 +0100

    Add FPS feature with image constantly changing

--- Editor3D/EditorForm.cs
+++ Editor3D/EditorForm.cs
@@ -12,15 +12,43 @@ namespace Editor3D
 {
     public partial class EditorForm : Form
     {
+        private const int FRAMES_PER_SECOND = 3;
+        private Color[] colors = { Color.Red, Color.Blue, Color.Green };
+        private int currentColorIndex = 0;
+
         public EditorForm()
         {
             InitializeComponent();
-            RenderGraphics();
+            RenderGraphicsPeriodically();
         }
 
-        private void RenderGraphics()
+        private void RenderGraphicsPeriodically()
         {
-            throw new NotImplementedException();
+            Timer timer = new Timer();
+            int millisecondsInOneSecond = 1000;
+            timer.Interval = millisecondsInOneSecond / FRAMES_PER_SECOND;
+            timer.Tick += RenderGraphics;
+            timer.Start();
+        }
+
+        private void RenderGraphics(object sender, EventArgs e)
+        {
+            int bitmapWidth = pictureBox1.Width;
+            int bitmapHeight = pictureBox1.Height;
+            Bitmap bitmap = new Bitmap(bitmapWidth, bitmapHeight);
+            for (int i = 0; i < bitmapWidth; ++i)
+            {
+                for (int j = 0; j < bitmapHeight; ++j)
+                {
+                    bitmap.SetPixel(i, j, colors[currentColorIndex]);
+                }
+            }
+            ++currentColorIndex;
+            if (currentColorIndex > 2)
+            {
+                currentColorIndex = 0;
+            }
+            pictureBox1.Image = bitmap;
         }
     }
 }
