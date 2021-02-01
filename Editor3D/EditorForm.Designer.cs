626db77242f43007fc7b96b9739c6af94caf97bb
Robert Pniewski <extrateo17@gmail.com>
2021-02-01 16:13:33 +0100

    Initialize repository with project files. Add initial GUI.

---
+++ Editor3D/EditorForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Editor3D
+{
+    partial class EditorForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.lightsPage = new System.Windows.Forms.TabPage();
+            this.shadingPage = new System.Windows.Forms.TabPage();
+            this.camerasPage = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tabControl1, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(776, 426);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(537, 420);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.lightsPage);
+            this.tabControl1.Controls.Add(this.shadingPage);
+            this.tabControl1.Controls.Add(this.camerasPage);
+            this.tabControl1.Location = new System.Drawing.Point(546, 3);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(227, 420);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // lightsPage
+            // 
+            this.lightsPage.Location = new System.Drawing.Point(4, 24);
+            this.lightsPage.Name = "lightsPage";
+            this.lightsPage.Padding = new System.Windows.Forms.Padding(3);
+            this.lightsPage.Size = new System.Drawing.Size(219, 392);
+            this.lightsPage.TabIndex = 0;
+            this.lightsPage.Text = "Lights";
+            this.lightsPage.UseVisualStyleBackColor = true;
+            // 
+            // shadingPage
+            // 
+            this.shadingPage.Location = new System.Drawing.Point(4, 24);
+            this.shadingPage.Name = "shadingPage";
+            this.shadingPage.Padding = new System.Windows.Forms.Padding(3);
+            this.shadingPage.Size = new System.Drawing.Size(219, 392);
+            this.shadingPage.TabIndex = 1;
+            this.shadingPage.Text = "Shading";
+            this.shadingPage.UseVisualStyleBackColor = true;
+            // 
+            // camerasPage
+            // 
+            this.camerasPage.Location = new System.Drawing.Point(4, 24);
+            this.camerasPage.Name = "camerasPage";
+            this.camerasPage.Size = new System.Drawing.Size(219, 392);
+            this.camerasPage.TabIndex = 2;
+            this.camerasPage.Text = "Cameras";
+            // 
+            // EditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(600, 400);
+            this.Name = "EditorForm";
+            this.Text = "Editor 3D";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage lightsPage;
+        private System.Windows.Forms.TabPage shadingPage;
+        private System.Windows.Forms.TabPage camerasPage;
+    }
+}
+
---
+++ Editor3D/EditorForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Editor3D
+{
+    public partial class EditorForm : Form
+    {
+        public EditorForm()
+        {
+            InitializeComponent();
+            RenderGraphics();
+        }
+
+        private void RenderGraphics()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
